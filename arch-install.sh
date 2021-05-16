41eef78178690343341c704060c8690cedb38725
h31sn0w <56997950+h31sn0w@users.noreply.github.com>
2021-05-16 21:28:25 +0200

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,56 +1,86 @@
 #!/bin/sh
 
-# CONFIGURATION
-read -p "drive (/dev/disk): " DRIVE
-read -p "host: " HOSTNAME
-read -p "encrytion? (y/n): " ENCRYPTION
-read -p "user: " USER_NAME
-TIMEZONE='Europe/Madrid'
-KEYMAP='us'
+config() {
+    read -p "drive (/dev/disk): " DRIVE
+    HOSTNAME='lemon'
+    USER_NAME='h31s'
+    TIMEZONE='Europe/Madrid'
+    KEYMAP='us'
+}
 
-# Preparing the disk
-echo "paritioning $DRIVE"
-parted --script $DRIVE \
-	mklabel gpt \
+prepare_disk() {
+    echo "paritioning $DRIVE"
+    parted --script $DRIVE \
+        mklabel gpt \
 	mkpart "root partition" ext4 512MiB 100% \
 	mkpart "EFI system partition" fat32 1MiB 512MiB \
 	set 2 esp on
-echo "formating $DRIVE paritions"
-mkfs.ext4 "$DRIVE"1
-mkfs.fat -F 32 "$DRIVE"2
-echo "mounting $DRIVE partitions"
-mkdir -p /mnt
-mount "$DRIVE"1 /mnt
-mkdir -p /mnt/boot
-mount "$DRIVE"2 /mnt/boot
+    echo "encrypting $DRIVE root partition"
+    cryptsetup luksFormat "$DRIVE"1
+    cryptsetpu luksOpen "$DRIVE"1 h31s
+    echo "formating $DRIVE paritions"
+    mkfs.ext4 -O "^has_journal" /dev/mapper/h31s
+    mkfs.fat -F 32 "$DRIVE"2
+    echo "mounting $DRIVE partitions"
+    mkdir -p /mnt
+    mount /dev/mapper/h31s /mnt
+    mkdir -p /mnt/boot
+    mount "$DRIVE"2 /mnt/boot
+}
 
-# Install
-pacstrap /mnt linux linux-firmware base base-devel neovim networkmanager
+pacstrap() {
+    pacstrap /mnt linux linux-firmware base base-devel neovim networkmanager
+}
 
-# Configuring arch
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "KEYMAP=us" > /etc/vconsole.conf
-echo "$HOSTNAME" > /etc/hostname
-cat > /etc/hosts <<EOF
+chroot() {
+    cp $0 /mnt/arch-install.sh
+    arch-chroot /mnt ./arch-install.sh chroot
+}
+    
+configuracion() {
+    ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    hwclock --systohc
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+    echo "KEYMAP=us" > /etc/vconsole.conf
+    echo "$HOSTNAME" > /etc/hostname
+    cat > /etc/hosts <<EOF
 127.0.0.1 localhost
 ::1       localhost
 EOF
-systemctl enable NetworkManager
-grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
-grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+    systemctl enable NetworkManager
+    grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
+    grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+}
 
-# Usb tweaks
+usb_tweaks() {
+    sed -i 's/HOOKS=(base udev autodetect modconf block filesystem keyboard fsck)/HOOKS=(base udev block keyboard autodetect modconf filesystem fsck)/g' /etc/mkinitcpio.conf
+    mkinitcpio -p linux
+    sed -i 's/#Storage=auto/Storage=volatile/g' /etc/systemd/journald.conf
+    sed -i 's/#SystemMaxUse=/SystemMaxUse=30M/g' /etc/systemd/journald.conf
+}
 
-# Install Paru
-cd /tmp || exit 1
-rm -rf /tmp/paru
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg --noconfirm -si
-cd /
+install_paru() {
+    cd /tmp
+    rm -rf /tmp/paru
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg --noconfirm -si
+    cd /
+}
+
+set -ex
+
+if [ "$1" == "chroot" ]
+then
+    configuracion
+    usb_tweaks
+    install_paru
+else
+    config
+    prepare_disk
+    pacstrap
+    genfstab -U /mnt >> /mnt/etc/fstab
+    chroot
+fi
